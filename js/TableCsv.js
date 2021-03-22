4e86b499a588fb29f24814850085ca2884640a85
Katsushige Fukushima <sasnorth_sports@outlook.jp>
2021-03-23 01:27:32 +0900

    master

---
+++ js/TableCsv.js
@@ -0,0 +1,10 @@
+export default class {
+    /**
+     * 
+     * @param {HTMLTableElement} root The table element which will display the CSV data.
+     */
+    constructor(root) {
+        this.root = root;
+        console.log("I am constructed!");
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import TableCsv from "./TableCsv.js";
+
+const tableRoot = document.querySelector("csvRoot");
+const tableCsv = new TableCsv(tableRoot);
