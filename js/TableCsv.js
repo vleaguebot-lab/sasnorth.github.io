4ceb805dd334081bd421f9d93e16cb2de979836c
Katsushige Fukushima <sasnorth_sports@outlook.jp>
2021-03-31 17:12:12 +0900

    master

--- js/TableCsv.js
+++ js/TableCsv.js
@@ -5,6 +5,41 @@ export default class {
      */
     constructor(root) {
         this.root = root;
-        console.log("I am constructed!");
+        // console.log("I am constructed!");
+    }
+
+    /**
+     * 
+     * @param {string[]} headerColumns List of headings to be used
+     */
+    setHeader(headerColumns) {
+        this.root.insertAdjacentHTML("afterbegin", 
+            `<thead>
+                <tr>
+                    ${headerColumns.map(text => `<th>${text}</th>`).join("")}
+                </tr>
+            </thead>`
+        );
+    }
+
+    /**
+     * 
+     * @param {string[][]} data A 2D array of data to be used as the table body
+     */
+    setBody(data) {
+        const rowsHtml = data.map(row => {
+            return
+                `<tr>
+                    ${ row.map(text => `<td>${ text }</td>` ).join("")}
+                </tr>`
+            ;
+        });
+
+        // // console.log(rowsHtml);
+        // this.root.insertAdjacentHTML("beforeend", `
+        //     <tbody>
+        //         ${ rowsHtml.join("") }
+        //     </tbody>
+        // `);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 import TableCsv from "./TableCsv.js";
 
-const tableRoot = document.querySelector("csvRoot");
+const tableRoot = document.querySelector("#csvRoot");
 const tableCsv = new TableCsv(tableRoot);
+
+tableCsv.setHeader(["ID", "Name", "Age"]);
+tableCsv.setBody([
+    [4500, "dom", 35],
+    [4500, "dom", 35],
+    [4500, "dom", 35]
+]);
