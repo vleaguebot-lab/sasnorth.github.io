35ee9f67f316db3625ba25ee5b9bbf7c1812792b
Katsushige Fukushima <sasnorth_sports@outlook.jp>
2021-04-04 01:31:31 +0900

    main

--- js/csv.js
+++ js/csv.js
@@ -16,14 +16,27 @@ function csv_array(data) {
     for (let i = 0; i < dataString.length; i++) { //あるだけループ
         dataArray[i] = dataString[i].split(',');
     }
+    // console.log(dataArray);
     let insertElement = ''; //テーブルタグに表示する用の変数
-    dataArray.forEach((element) => { //配列の中身を表示
+    insertElement += '</tr>';
+    for (let j = 0; j < dataString.length; j++) {
         insertElement += '<tr>';
-        element.forEach((childElement) => {
-            insertElement += `<td>${childElement}</td>`
+        dataArray[j].forEach((childElement) => {
+            if (j == 0) {
+                insertElement += `<th>${childElement}</th>`
+            } else {
+                insertElement += `<td>${childElement}</td>`;
+            }
         });
         insertElement += '</tr>';
-    });
+    };
+    // dataArray.forEach((element) => { //配列の中身を表示
+    //     insertElement += '<tr>';
+    //     element.forEach((childElement) => {
+    //         insertElement += `<td>${childElement}</td>`
+    //     });
+    //     insertElement += '</tr>';
+    // });
     output_csv.innerHTML = insertElement; // 表示
 }
 csv_data(`${dir}data/${division}/${csv_name}.csv`); // csvのパス
